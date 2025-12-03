a0fb50e842942cd870873f4af543324bccadb229
blkgoose <alessio.biancone@gmail.com>
2025-12-03 13:33:43 +0100

    feat: add day switch for main

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,21 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+
 import Day1
 
 main :: IO ()
 main = do
-    contents <- readFile "app/day1.input"
-    let inputLines = lines contents :: [String]
-    print $ Day1.execute inputLines
+    args <- getArgs
+    case args of
+        (day:_) -> do 
+            input <- lines <$> readFile ("app/day" ++ day ++ ".input")
+            case day of
+                "1" -> day1 input
+                _   -> putStrLn "Day not implemented."
+        _     -> putStrLn "Wrong number of arguments. Usage: run <day>"
+
+
+day1 :: [String] -> IO ()
+day1 input = do
+    print $ Day1.execute input

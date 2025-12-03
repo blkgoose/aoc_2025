825664024f6e55431c1f2a0002ab88408fcefcab
blkgoose <alessio.biancone@gmail.com>
2025-12-03 14:07:05 +0100

    refactor(test): explicitly use day 1 things

--- tests/Main.hs
+++ tests/Main.hs
@@ -9,24 +9,24 @@ main = hspec $ do
 
 day1Spec :: Spec
 day1Spec = do
-  describe "gives the password" $ do
+  describe "day 1 tests" $ do
     it "returns 0 if it never stops on 0" $ do
-      fst (execute ["L10", "R20"]) `shouldBe` 0
+      fst (Day1.execute ["L10", "R20"]) `shouldBe` 0
 
     it "returns 1 if it stops on 0 once" $ do
-      fst (execute ["L50"]) `shouldBe` 1
+      fst (Day1.execute ["L50"]) `shouldBe` 1
 
     it "returns 2 if it stops on 0 twice" $ do
-      fst (execute ["L50", "R50", "L50"]) `shouldBe` 2
+      fst (Day1.execute ["L50", "R50", "L50"]) `shouldBe` 2
 
     it "solves the test input" $ do
-      fst (execute ["L68", "L30", "R48", "L5 ", "R60", "L55", "L1 ", "L99", "R14", "L82"]) `shouldBe` 3
+      fst (Day1.execute ["L68", "L30", "R48", "L5 ", "R60", "L55", "L1 ", "L99", "R14", "L82"]) `shouldBe` 3
 
     it "returns 1 if it goes through 0 once" $ do
-      snd (execute ["R60"]) `shouldBe` 1
+      snd (Day1.execute ["R60"]) `shouldBe` 1
 
     it "solves the test input for part 2" $ do
-      snd (execute ["L68", "L30", "R48", "L5 ", "R60", "L55", "L1 ", "L99", "R14", "L82"]) `shouldBe` 6
+      snd (Day1.execute ["L68", "L30", "R48", "L5 ", "R60", "L55", "L1 ", "L99", "R14", "L82"]) `shouldBe` 6
 
     it "returns 2 if it goes through 0 twice in 1 turn" $ do
-      snd (execute ["R150"]) `shouldBe` 2
+      snd (Day1.execute ["R150"]) `shouldBe` 2

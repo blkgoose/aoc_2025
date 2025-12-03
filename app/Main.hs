798baede67dced3f40662b0730dd2bff1f9321e8
blkgoose <alessio.biancone@gmail.com>
2025-12-03 13:50:02 +0100

    feat: add missing day handler

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import System.Environment (getArgs)
+import Control.Exception (catch)
 
 import Day1
 
@@ -9,12 +10,17 @@ main = do
     args <- getArgs
     case args of
         (day:_) -> do 
-            input <- lines <$> readFile ("app/day" ++ day ++ ".input")
+            content <- catch (readFile $ "app/day" ++ day ++ ".input") ignore
+            input <- return $ lines content
+
             case day of
                 "1" -> day1 input
                 _   -> putStrLn "Day not implemented."
         _     -> putStrLn "Wrong number of arguments. Usage: run <day>"
 
+
+ignore :: IOError -> IO String
+ignore _ = return ""
 
 day1 :: [String] -> IO ()
 day1 input = do

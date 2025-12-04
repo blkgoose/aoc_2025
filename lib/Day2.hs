b14e2db983a5bb5ef65df27da7a142af9efa0aa2
blkgoose <alessio.biancone@gmail.com>
2025-12-04 08:43:56 +0100

    feat: solves day 2 partial part 2 input

--- lib/Day2.hs
+++ lib/Day2.hs
@@ -2,24 +2,40 @@ module Day2 (execute) where
 
 import Data.List.Split (splitOn)
 import Utils (trace, range)
+import qualified Debug.Trace as Debug
 
 execute :: String -> (Int, Int)
-execute input = do
+execute input =
     let groups = splitOn "," input
         allGroups = map (map read . splitOn "-") groups :: [[Int]]
+        rangeIds = concatMap (\[start, end] -> range start end) allGroups
 
-        rangeIds = concatMap (\r ->
-                       let (start, end) = makeTuple r
-                       in range start end
-                   ) allGroups
+        part1 = sum $ filter isInvalid rangeIds
+        part2 = sum $ filter isInvalid' rangeIds
 
-    (sum $ filter isInvalid rangeIds, 0)
+    in (part1, part2)
 
-makeTuple :: [Int] -> (Int, Int)
-makeTuple [a, b] = (a, b)
+isInvalid' :: Int -> Bool
+isInvalid' id =
+    let id' = show id
+        halfId = take (length id' `div` 2) id'
+
+    in something halfId id'
+
+
+something :: [Char] -> String -> Bool
+something [] _ = False
+something block match =
+    let repeatFor = length match `div` length block
+        block' = concat $ replicate repeatFor block
+    in
+        if block' == match then
+            True
+        else
+            something (init block) match
 
 isInvalid :: Int -> Bool
-isInvalid id = 
+isInvalid id =
     let digits = show id
         (left, right) = splitAt (length digits `div` 2) digits
     in left == right

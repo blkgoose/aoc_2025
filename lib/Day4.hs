ac16475c1122ce6e28cb2fe8086fb375332739e9
blkgoose <alessio.biancone@gmail.com>
2025-12-09 12:58:30 +0100

    feat(day5): solves part 1

--- lib/Day4.hs
+++ lib/Day4.hs
@@ -4,20 +4,18 @@ import Utils (groupWhile, trim, trace, range)
 import Data.Char (isSpace)
 import Data.List.Split (splitOn)
 
-import Debug.Trace as Debug
-
 import Flow
 
 execute :: [String] -> (Int, Int)
 execute input =
     let [database', ingredients'] = groupWhile (not . null) input
         ingredients = map read ingredients' :: [Int]
-        database = concat $ map explode database' :: [Int]
-    in ( filter (`elem` database) ingredients |> length
+        database = map (splitOn "-") database' |> map (\[a,b] -> (read a, read b)) :: [(Int, Int)]
+    in ( filter (isInRange database) ingredients |> length
        , 0
        )
 
-explode :: String -> [Int]
-explode line =
-    let [start, end] = splitOn "-" line
-    in range (read start) (read end)
+isInRange :: [(Int, Int)] -> Int -> Bool
+isInRange database n =
+    database
+        |> any (\(a,b) -> n >= a && n <= b)

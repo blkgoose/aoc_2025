44e2646bf3c9c3c87fd43e6e19dfe038e779e191
blkgoose <alessio.biancone@gmail.com>
2025-12-14 18:30:40 +0100

    feat(day9): solves part 2

--- lib/Day9.hs
+++ lib/Day9.hs
@@ -1,8 +1,9 @@
 module Day9 (execute) where
 
-import Data.List (singleton, sortBy, sortOn, sort)
-import Data.List.Split (splitOn, chunksOf)
-import qualified Data.HashSet as HS
+import Data.List (find, singleton, sort, sortBy, sortOn)
+import Data.List.Split (chunksOf, splitOn)
+import Data.Map ((!))
+import qualified Data.Map as M
 import Flow
 import Utils
 
@@ -23,62 +24,53 @@ execute input =
 part1 :: [Point] -> Int
 part1 points =
   orderByLargestArea points
-    |> \((_, _, area):_) -> area
+    |> \((_, _, area) : _) -> area
 
 part2 :: [Point] -> Int
 part2 points =
   let poly = polygon points
-      filledPoly = fillPolygon poly
+
+      (minY, maxY) =
+        points
+          |> map snd
+          |> (\ys -> (minimum ys, maximum ys))
+
+      cache =
+        [minY .. maxY]
+          |> map
+            ( \y ->
+                ( y,
+                  filter
+                    ( \((ax, ay), (bx, by)) ->
+                        if ax == bx then y > min ay by && y < max ay by -- strict, no vertical lines vertices
+                        else y >= min ay by && y <= max ay by -- inclusive of horizontal lines
+                    )
+                    poly
+                )
+            )
+          |> Utils.traceList "Cache: "
+          |> M.fromList
    in orderByLargestArea points
-    |> filter (\(a, b, _) -> [a, b] |> all (`HS.member` filledPoly))
-    |> \((_, _, area):_) -> area
-
-fillPolygon :: [(Point, Point)] -> HS.HashSet Point
-fillPolygon edges =
-  let filledEdges = HS.fromList $ concatMap fillEdge edges
-
-      points = map fst edges
-      minX = minimum $ map fst points
-      maxX = maximum $ map fst points
-      minY = minimum $ map snd points
-      maxY = maximum $ map snd points
-
-      allPoints = [(x, y) | x <- [minX .. maxX], y <- [minY .. maxY]] |> Utils.traceList "All points:"
-
-      filled = allPoints
-        |> foldl (accInsidePoints filledEdges) ([], 0)
-        |> fst
-        |> HS.fromList
-      filledAndNotFilled = allPoints
-          |> map (\p ->
-              if p `HS.member` filledEdges then "E"
-              else if p `HS.member` filled then "#"
-              else " "
+        |> find
+          ( \(a, b, _) ->
+              vertices a b |> Utils.trace_ "Checking: "
+                |> all
+                  ( \(x, y) ->
+                      let segOnLine = cache ! y
+                          left = segOnLine |> filter (\((ax, _), (bx, _)) -> x < min ax bx) |> length |> Utils.trace_ ("Left at " ++ show (x, y) ++ ": ")
+                          right = segOnLine |> filter (\((ax, _), (bx, _)) -> x > max ax bx) |> length |> Utils.trace_ ("Right at " ++ show (x, y) ++ ": ")
+                          res = not (even left && even right)
+                       in Utils.trace res
+                  )
           )
-          |> chunksOf (maxY - minY + 1)
-          |> Utils.traceList "Filled and not filled points:"
-   in Utils.trace' filledAndNotFilled filled
+        |> (\x -> case x of
+              Just (a, b, area) -> Utils.trace' ("Found between " ++ show a ++ " and " ++ show b ++ " with area " ++ show area) area
+              Nothing -> 0
+           )
   where
-    accInsidePoints :: HS.HashSet Point -> ([Point], Int) -> Point -> ([Point], Int)
-    accInsidePoints edges (set, edgeCount) point =
-      let onEdge = HS.member point edges
-          wasRidingTheEdge = edgeCount > 1
-          isInside = odd edgeCount
-      in case (isInside, onEdge) of
-        (False, True)  -> (point:set, edgeCount + 1)
-        (True, True)    -> (point:set, edgeCount)
-        (False, False)  -> (set, edgeCount)
-        (True, False)   -> (point:set, edgeCount)
-        _ -> (set, edgeCount)
-
-    fillEdge :: (Point, Point) -> [Point]
-    fillEdge ((x1, y1), (x2, y2)) =
-      let minX = min x1 x2
-          maxX = max x1 x2
-          minY = min y1 y2
-          maxY = max y1 y2
-       in [(x, y) | x <- [minX .. maxX], y <- [minY .. maxY]]
-
+    vertices :: Point -> Point -> [Point]
+    vertices a b =
+      [a, b, ((fst a, snd b)), ((fst b, snd a))]
 
 polygon :: [Point] -> [(Point, Point)]
 polygon points@(x : t) = zip points (t ++ [x])
